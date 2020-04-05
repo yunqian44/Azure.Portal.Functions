2a91f7294b9d5b0a3512e305d0a0a08b605ab316
yunnqian44 <yunqian8@live.com>
2020-04-05 12:00:12 +0800

    添加项目文件。

---
+++ Azure.Protal.Function.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Azure.Protal.Function.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
